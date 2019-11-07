c03e3e32cdfe43eb9c7fa8efa3995c7290aaff3b
Mateusz <wasik.matt@gmail.com>
2019-11-07 20:38:30 +0100

    CC-14-function-for-select-component (#19)
    * added function for select component
    
    * cr fix

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,119 +1,94 @@
-import {Button, Input, Select} from "antd"
-import 'antd/dist/antd.css';
+import {Button, Input} from "antd"
+import 'antd/dist/antd.css'
 import './Dashboard.scss'
-import React, {useEffect, useState} from "react";
-import {getCurrencyValues} from "../services/CurrencyService";
+import React, {useEffect, useState} from "react"
+import {getCurrencyValues} from "../services/CurrencyService"
+import {CurrencySelect} from './CurrecySelectComponent/SelectComponent'
 
-const {Option} = Select;
 export const regex = new RegExp("^[0-9]*$")
 
 export const Dashboard = () => {
 
-  const [apiRates, setApiRates] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState(undefined);
-  const [toCurrency, setToCurrency] = useState(undefined);
-  const [userValue, setUserValue] = useState(0);
-  const [converterValue, setConvertedValue] = useState(0);
-  const [date, setCurrentDate] = useState('');
+  const [apiRates, setApiRates] = useState([])
+  const [fromCurrency, setFromCurrency] = useState(undefined)
+  const [toCurrency, setToCurrency] = useState(undefined)
+  const [userValue, setUserValue] = useState(0)
+  const [converterValue, setConvertedValue] = useState(0)
+  const [date, setCurrentDate] = useState('')
 
-    useEffect(() => {
-      getCurrencyValues().then(array => {
-        setApiRates(array[0].rates);
-        setCurrentDate(array[0].effectiveDate);
-      })
-    }, []);
+  useEffect(() => {
+    getCurrencyValues().then(array => {
+      setApiRates(array[0].rates)
+      setCurrentDate(array[0].effectiveDate)
+    })
+  }, [])
 
 
-    useEffect(() => {
-      const firstValue = apiRates.find(rate => rate.code === fromCurrency);
-      const secondValue = apiRates.find(rate => rate.code === toCurrency);
+  useEffect(() => {
+    const firstValue = apiRates.find(rate => rate.code === fromCurrency)
+    const secondValue = apiRates.find(rate => rate.code === toCurrency)
 
-      if (firstValue && secondValue) {
-        const result = firstValue.bid / secondValue.bid;
+    if (firstValue && secondValue) {
+      const result = firstValue.bid / secondValue.bid
 
-        setConvertedValue((userValue * result).toFixed(2).toString());
-      }}, [apiRates, fromCurrency, toCurrency, userValue]);
-
-    const onChangeValue = (value) => {
-      if (regex.test(value.target.value))
-        setUserValue(value.target.value)
+      setConvertedValue((userValue * result).toFixed(2).toString())
     }
+  }, [apiRates, fromCurrency, toCurrency, userValue])
 
-    const onCurrencySwap = () => {
-      if (!fromCurrency || !toCurrency) return
-      const temporaryFromCurrencyKeeper = fromCurrency
-      setFromCurrency(toCurrency)
-      setToCurrency(temporaryFromCurrencyKeeper)
-    }
+  const onChangeValue = (value) => {
+    if (regex.test(value.target.value))
+      setUserValue(value.target.value)
+  }
 
-    return (
+  const onCurrencySwap = () => {
+    if (!fromCurrency || !toCurrency) return
+    const temporaryFromCurrencyKeeper = fromCurrency
+    setFromCurrency(toCurrency)
+    setToCurrency(temporaryFromCurrencyKeeper)
+  }
+
+  return (
       <div className="converter-container">
         <div className="selector-wrapper">
 
-          <Select
-            showSearch
-            allowClear={true}
-            value={fromCurrency}
-            style={{width: 200}}
-            placeholder="Select currency"
-            optionFilterProp="children"
-            onChange={setFromCurrency}
-            filterOption={(input, option) =>
-              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-          >
-            {renderOptionsInSelector(apiRates)}
-          </Select>
+          <CurrencySelect
+              value={fromCurrency}
+              onChange={setFromCurrency}
+              currencyRates={apiRates}/>
 
           <div className="source-input">
             <Input
-              value={userValue}
-              onChange={onChangeValue}/>
+                value={userValue}
+                onChange={onChangeValue}/>
           </div>
 
           <div className="replace-currency-button">
             <Button
-              type="primary"
-              icon="swap"
-              onClick={onCurrencySwap}/>
+                type="primary"
+                icon="swap"
+                onClick={onCurrencySwap}/>
           </div>
 
           <div className="destination-input">
             <Input
-              value={converterValue}/>
+                value={converterValue}/>
           </div>
 
-          <Select
-            showSearch
-            allowClear={true}
-            value={toCurrency}
-            style={{width: 200}}
-            placeholder="Select currency"
-            optionFilterProp="children"
-            onChange={setToCurrency}
-            filterOption={(input, option) =>
-              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-          >
-            {renderOptionsInSelector(apiRates)}
-          </Select>
+          <CurrencySelect
+              value={toCurrency}
+              onChange={setToCurrency}
+              currencyRates={apiRates}/>
         </div>
 
         <div className="effectiveDate">
           {displayDateInformation(date)}
         </div>
       </div>
-    )
-  };
+  )
+}
 
-  const renderOptionsInSelector = (rates) => {
-    return rates.map(rate => {
-      return <Option value={rate.code} key={rate.code}>{rate.currency}</Option>
-    })
-  };
-
-  const displayDateInformation = (date) => {
-    return (
+const displayDateInformation = (date) => {
+  return (
       <h4>{date}</h4>
-    )
-  };
+  )
+}
