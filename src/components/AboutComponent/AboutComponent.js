d3fbb43db44acebb02e7ee43caa28836a17d63bd
Radomir Skrzepij <zyziu10@gmail.com>
2020-01-25 10:30:54 +0100

    CC-34 Add og tags for facebook (#38)
    * Add og tags for facebook
    
    * CR fixes

--- src/components/AboutComponent/AboutComponent.js
+++ src/components/AboutComponent/AboutComponent.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './AboutComponent.scss'
+import { Helmet } from 'react-helmet'
 
 const About = () => {
   return (
     <div className="about">
+      <Helmet>
+        <title>CConverter - O projekcie</title>
+      </Helmet>
+      
       <h1>O Projekcie CConverter</h1>
     </div>
   )
--- src/components/ContactComponent/ContactComponent.js
+++ src/components/ContactComponent/ContactComponent.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './ContactComponent.scss'
+import { Helmet } from 'react-helmet'
 
 const Contact = () => {
   return (
     <div className="contact">
+  
+      <Helmet>
+        <title>CConverter - Kontakt</title>
+      </Helmet>
+      
       <div className="contact__form">
         Formularz kontaktowy
       </div>
