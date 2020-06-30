0784dba39a7cae820e9387bde73d7634f166e920
MateuszWasik <wasik.matt@gmail.com>
2020-06-30 23:09:45 +0200

    add about view, fotos and rwd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.scss'
-import { ApiRate, Dashboard } from './components/Dashboard'
+import {ApiRate, Dashboard} from './components/Dashboard'
 import MainHeader from './components/MainHeaderComponent/MainHeaderComponent'
-import { Layout, Spin } from 'antd'
+import {Layout, Spin} from 'antd'
 
 import About from './components/AboutComponent/AboutComponent'
 import Contact from './components/ContactComponent/ContactComponent'
 import UndefinedRoute from './components/UndefinedRouteComponent/UndefinedRouteComponent'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { getCurrencyValues } from './services/CurrencyService'
-import { from } from 'rxjs'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {getCurrencyValues} from './services/CurrencyService'
+import {from} from 'rxjs'
 
 const {Header, Content} = Layout
 
@@ -17,65 +17,69 @@ const resource = getCurrencyValues()
 const $resourceObservable = from(resource)
 
 type Response = {
-  rates: ApiRate[];
-  date: string
+    rates: ApiRate[];
+    date: string
 }
 
 const App: React.FC = () => {
 
-  const [apiRates, setApiRates] = useState<ApiRate[]>([])
-  const [date, setCurrentDate] = useState('')
-  const [dataReady, setDataReady] = useState(false)
+    const [apiRates, setApiRates] = useState<ApiRate[]>([])
+    const [date, setCurrentDate] = useState('')
+    const [dataReady, setDataReady] = useState(false)
 
-  useEffect(() => {
-    $resourceObservable.subscribe((response : Response) => {
-      setApiRates(response.rates)
-      setCurrentDate(response.date)
-      setDataReady(true)
-    })
-  }, [])
+    useEffect(() => {
+        $resourceObservable.subscribe((response: Response) => {
+            setApiRates(response.rates)
+            setCurrentDate(response.date)
+            setDataReady(true)
+        })
+    }, [])
 
-  return (
-    <div className="cc-theme-purple">
-      <Router basename={process.env.PUBLIC_URL}>
-        <Layout className="main-layout">
-          <Header className="main-layout__header">
-            <MainHeader/>
-          </Header>
+    return (
+        <div className="cc-theme-purple">
+            <Router basename={process.env.PUBLIC_URL}>
+                <Layout className="main-layout">
+                    <Header className="main-layout__header">
+                        <MainHeader/>
+                    </Header>
 
-          <Switch>
-            <Route
-              exact
-              path="/"
-            >
-              <Content className="main-layout__content">
-                {!dataReady
-                  ? <Spin/>
-                  : <Dashboard
-                  rates={apiRates}
-                  date={date}
-                />
-                }
-              </Content>
-            </Route>
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                        >
+                            <Content className="main-layout__content">
+                                {!dataReady
+                                    ? <Spin/>
+                                    : <Dashboard
+                                        rates={apiRates}
+                                        date={date}
+                                    />
+                                }
+                            </Content>
+                        </Route>
 
-            <Route path="/about">
-              <About/>
-            </Route>
+                        <Route path="/about">
+                            {!dataReady
+                                ? <Spin/>
+                                :
+                                <About/>
+                            }
+                        </Route>
 
-            <Route path="/contact">
-              <Contact/>
-            </Route>
+                        <Route path="/contact">
+                            <Contact/>
+                        </Route>
 
-            <Route path="*">
-              <UndefinedRoute/>
-            </Route>
-          </Switch>
+                        <Route path="*">
+                            <UndefinedRoute/>
+                        </Route>
+                    </Switch>
 
-        </Layout>
-      </Router>
-    </div>
-  )
+                </Layout>
+            </Router>
+        </div>
+    )
 }
 
 export default App
--- src/components/AboutComponent/AboutComponent.tsx
+++ src/components/AboutComponent/AboutComponent.tsx
@@ -1,55 +1,58 @@
 import React from 'react'
 import './AboutComponent.scss'
-import { Helmet } from 'react-helmet'
+import {Helmet} from 'react-helmet'
 
 const About: React.FC = () => {
-  return (
-    <div className="about">
-      <Helmet>
-        <title>CConverter - O projekcie</title>
-      </Helmet>
+    return (
+        <div className="about">
+            <Helmet>
+                <title>CConverter - O projekcie</title>
+            </Helmet>
 
-      <div className="about-layout">
-        <div className="about-layout__info">
-          <h3>CConverter to open-sourcowy projekt stworzony przez Codrill. Mając do dyspozycji 150 walut z całego
-            świata przeliczenia stają się banalnie proste. Wystarczy, że wybierzesz waluty, które Cię interesują oraz
-            wpiszesz
-            liczbę. To wszystko. Aplikacja automatycznie przeliczy podaną przez Ciebie kwotę po odpowiednim i oficjalnym
-            kursie
-            wymiany.
-          </h3>
+            <div className="about-layout">
+                <div className="about-layout__wrapper">
+                    <div className="about-layout__info">
+                        <p><strong>CConverter</strong> to open-sourcowy projekt stworzony przez <strong>Codrill</strong>.
+                            Mając do dyspozycji 150 walut z
+                            całego świata przeliczenia stają się banalnie proste.</p>
+                        <p>Wystarczy, że wybierzesz waluty które Cię interesują oraz wpiszesz
+                            liczbę. To wszystko. Aplikacja automatycznie przeliczy podaną przez Ciebie kwotę po
+                            odpowiednim i oficjalnym kursie wymiany.
+                        </p>
+                    </div>
+
+                    <div className="about-layout__foto dollar-foto">
+
+                    </div>
+                </div>
+                <div className="about-layout__wrapper second-info">
+                    <div className="about-layout__foto bank-foto">
+                    </div>
+
+                    <div className="about-layout__info">
+                        <p>Żeby zagwarantować naszym użytkownikom pewne i oficjalne kursy wymiany używamy przeliczników
+                            podanych przez <strong>Narodowy Bank Polski</strong> który w każdy dzień roboczy aktualizuje
+                            swoje najpopularniejsze
+                            waluty. Te bardziej egzotyczne aktualizowane są co środę.</p>
+                    </div>
+                </div>
+
+                <div className="about-layout__wrapper">
+                    <div className="about-layout__info">
+                        <p>Nasz konwerter jest przystosowany do działania na każdym urządzeniu. Potrzebujesz coś szybko
+                            przeliczyć? Aplikacja działa wyśmienicie na telefonach komórkowych czy tabletach.</p>
+
+                        <p><strong>CConverter</strong> jest na bieżąco rozbudowywany oraz rozwijany.
+                            Zachęcamy do dzielenia się uwagami przez nasz formularz zgłoszeniowy.</p>
+                    </div>
+
+                    <div className="about-layout__foto rwd-foto">
+
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div className="about-layout__foto">
-
-        </div>
-
-        <div className="about-layout__info">
-          <p>Żeby zagwarantować naszym użytkownikom pewne i oficjalne kursy wymiany używamy przeliczników podanych przez
-            Narodowy Bank Polski, który w każdy dzień roboczy aktualizuje swoje najpopularniejsze waluty. Te
-            bardziej egzotyczne aktualizowane są co środę.</p>
-
-          <p>Do najpopularniejszych walut, aktualizowanych codziennie, zaliczamy:</p>
-        </div>
-
-      <div className="about-layout__foto">
-      </div>
-
-        <div className="about-layout__info">
-        <p>Nasz konwerter jest przystosowany do działania na każdym urządzeniu. Potrzebujesz coś szybko przeliczyć?
-          Aplikacja
-          działa wyśmienicie na telefonach komórkowych czy tabletach.</p>
-
-        <p>CConverter jest na bieżąco rozbudowywany oraz rozwijany. Zachęcamy do dzielenia się uwagami przez nasz
-          formularz zgłoszeniowy.</p>
-        </div>
-
-        <div className="about-layout__foto">
-
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default About
