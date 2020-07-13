eddb53e405734dfeede3df82eb0ea58814cce10f
Radomir Skrzepij <radek.s87@gmail.com>
2020-07-13 22:32:11 +0200

    update ant design to v4 and refactor contact form

--- src/components/ContactFormComponent/ContactFormComponent.tsx
+++ src/components/ContactFormComponent/ContactFormComponent.tsx
@@ -1,103 +1,72 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Input from "antd/lib/input";
 
 import './ContactFormComponent.scss'
 import Button from "antd/lib/button";
-import { Form, message } from "antd";
-import { emailRegex, usernameRegex } from "../../constants/Regex";
+import {Form, message} from "antd";
+import {emailRegex, usernameRegex} from "../../constants/Regex";
 import emailjs from 'emailjs-com';
-import { defaultService, templateId, userId } from "../../constants/EmailJS";
+import {defaultService, templateId, userId} from "../../constants/EmailJS";
+import {Store} from "antd/lib/form/interface";
 
+
+// Not used because for now there is no way to parametrize AntD form values.
+// ISSUES:
+// https://github.com/ant-design/ant-design/issues/21195
+// https://github.com/react-component/field-form/issues/70
+type FormValues = {
+  username: string
+  userEmail: string
+  userMessage: string
+}
 
 export const ContactForm: React.FC = () => {
-
-    const [hasUsernameError, setHasUsernameError] = useState<boolean>(false);
-    const [hasEmailError, setHasEmailError] = useState<boolean>(false);
-    const [firstInit, setFirstInit] = useState<boolean>(true);
-    const [username, setUsername] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [buttonDisabled, setButtonDisabled] = useState<boolean>(false)
-
-    const onUsernameChange = (inputElement: React.ChangeEvent<HTMLInputElement>) => {
-        validationCheck(inputElement.target.value, usernameRegex, setHasUsernameError, setUsername);
-    }
-
-    const onEmailChange = (inputElement: React.ChangeEvent<HTMLInputElement>) => {
-        validationCheck(inputElement.target.value, emailRegex, setHasEmailError, setEmail);
-    }
-
-    const validationCheck = (value: string, regex: RegExp, setHook: Function, setUserValue: Function) => {
-        if (firstInit) {
-            setFirstInit(false)
-        }
-        if (!value) {
-            setHook(true)
-            setUserValue('');
-        } else if (regex.test(value)) {
-            setUserValue(value);
-            setHook(false);
-        } else {
-            setUserValue(value);
-            setHook(true);
-        }
-    }
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false)
 
 
-    const submitForm = (e: any) => {
-        e.preventDefault();
+  const submitForm = (values: Store): void => {
+    setButtonDisabled(true)
 
-        if (firstInit) {
-            setHasUsernameError(true);
-            setHasEmailError(true);
-            return;
-        } else if (!firstInit && (hasEmailError || hasUsernameError)) { return; }
+    emailjs.send(defaultService, templateId, values, userId)
+      .then(message.loading('Wysyłanie wiadomości...'))
+      .then(() => {
+        message.success('Wysłane!', 10)
+      }, () => {
+        message.error('Coś poszło nie tak. Spróbuj ponownie', 10)
+      }).finally(() => setButtonDisabled(false));
+  }
 
-        setButtonDisabled(true)
-        emailjs.sendForm(defaultService, templateId, e.target, userId)
-            .then(message.loading('Wysyłanie wiadomości...'))
-            .then(() => {
-                message.success('Wysłane!', 10)
-                setButtonDisabled(false)
-            }, () => {
-                message.error('Coś poszło nie tak. Spróbuj ponownie', 10)
-                setButtonDisabled(false)
-            });
-    }
-
-    return (
-            <div className="form">
-                <Form id="contact-form" onSubmit={submitForm}>
-                    <Form.Item label="Imię i nazwisko"
-                               validateStatus={hasUsernameError ? 'error' : 'success'}
-                               help={hasUsernameError ? 'To pole musi być wypełnione' : ''}
-                               required={true}
-                    >
-                        <Input
-                            name="username"
-                            value={username}
-                            onChange={onUsernameChange}/>
-                    </Form.Item>
-                    <Form.Item label="E-mail"
-                               validateStatus={hasEmailError ? 'error' : 'success'}
-                               help={hasEmailError ? 'Podaj proszę poprawny adres email' : ''}
-                               required={true}>
-                        <Input
-                            name="userEmail"
-                            value={email}
-                            onChange={onEmailChange}/>
-                    </Form.Item>
-                    <Form.Item className="text-area" label="Napisz do nas">
-                        <Input.TextArea
-                            name="userMessage"/>
-                    </Form.Item>
-                    <Form.Item className="submitButton">
-                        <Button type="primary" htmlType="submit" disabled={buttonDisabled}>
-                            Wyślij
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </div>
-    )
+  return (
+    <div className="form">
+      <Form name="contactForm" id="contact-form" onFinish={submitForm} layout="vertical">
+        <Form.Item label="Imię i nazwisko" name="username" hasFeedback rules={[
+          {required: true, message: 'To pole musi być wypełnione'},
+          {pattern: usernameRegex, message: 'Podaj proszę poprawne imię i nazwisko'},
+          {min: 5, message: 'Podaj proszę poprawne imię i nazwisko'}
+        ]}>
+          <Input/>
+        </Form.Item>
+        <Form.Item label="E-mail" name="userEmail" hasFeedback rules={[
+          {required: true, message: 'To pole musi być wypełnione'},
+          {pattern: emailRegex, message: 'Podaj proszę poprawny adres email'}
+        ]}>
+          <Input/>
+        </Form.Item>
+        <Form.Item className="text-area" label="Napisz do nas" name="userMessage" hasFeedback rules={[
+          {required: true, message: 'To pole musi być wypełnione'},
+          {min: 20, message: 'Wiadomość powinna zawierać od 20 do 500 znaków'},
+          {max: 500, message: 'Wiadomość powinna zawierać od 20 do 500 znaków'}
+        ]}>
+          <Input.TextArea autoSize={{minRows: 7, maxRows: 7}}/>
+        </Form.Item>
+        <Form.Item className="submitButton">
+          <Button type="primary" htmlType="submit" disabled={buttonDisabled}>
+            Wyślij
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
 export default ContactForm;
