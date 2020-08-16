72995f0468447cab0854d4822152d418b861f032
MateuszWasik <wasik.matt@gmail.com>
2020-08-16 18:18:58 +0200

    cr fixes

--- src/components/ChartComponent/ChartComponent.tsx
+++ src/components/ChartComponent/ChartComponent.tsx
@@ -3,14 +3,14 @@ import { Line } from "react-chartjs-2";
 import { Spin } from "antd";
 import "./ChartComponent.scss"
 
-export const ChartComponent: React.FC<any> = (chartData) => {
+export const ChartComponent: React.FC<any> = ({chartData}) => {
 
     return (
         <div className="chart-container">
             {!!chartData ?
                 <div className="line-chart">
                     <Line
-                        data={chartData.chartData}
+                        data={chartData}
                         options={{
                             responsive: true,
                             maintainAspectRatio: false,
