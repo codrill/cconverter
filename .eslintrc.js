845adfc3e456626a4861fc273bb0892928d9b906
Radomir Skrzepij <zyziu10@gmail.com>
2021-03-18 11:39:28 +0100

    Update ESLint related libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'react-app', // used to enable import/order, import/extensions and more because .eslintrc is outside default CRA ESlint config (located in package.json)
   ],
   plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
