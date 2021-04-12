71456531479f785518027b38de1833cb8550df53
Radomir Skrzepij <zyziu10@gmail.com>
2021-04-12 08:05:52 +0200

    fix all existing eslint compolation warnings

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,6 +1,8 @@
-export const sort = (values: Array<any>, propertyToSortBy = '') => {
+export const sort = <T>(values: Record<string, T>[], propertyToSortBy = ''): Record<string, T>[] => {
   if (propertyToSortBy in values[0])
-    return values.sort((a, b) => (a[propertyToSortBy].toLowerCase() < b[propertyToSortBy].toLowerCase() ? -1 : 1))
+    return values.sort((a, b) => {
+      return String(a[propertyToSortBy]).toLowerCase() < String(b[propertyToSortBy]).toLowerCase() ? -1 : 1
+    })
 
   return values.sort((a, b) => (a < b ? -1 : 1))
 }
